2eb2776fc37987d87b293382fffe58f12ea80137
lishing <shingli.chan@gmail.com>
2023-08-28 16:01:24 +0800

    code along

---
+++ code-along/4-names.js
@@ -0,0 +1,7 @@
+//local
+const secret = 'SUPER SECRET'
+// share (can be access everywhere)
+const john = 'john'
+const peter = 'peter'
+
+module.exports = { john , peter }
---
+++ code-along/7-mind-grenade.js
@@ -0,0 +1,12 @@
+const num1 = 5;
+const num2 = 10;
+
+function addValues(){
+    console.log(`the sum is : ${num1 + num2}`)
+}
+
+// evoke the function
+addValues()
+
+// if there is a function within the module, it will execute/run on app.js 
+// without assigning to a module
